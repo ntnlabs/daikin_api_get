af82048fe2b02e40918e802c83ea3b8c69d80bfd
NTN Labs <ntn.labs@gmail.com>
2019-11-16 22:04:32 +0100

    final touches

--- daikin_parse.php
+++ daikin_parse.php
@@ -1,15 +1,23 @@
 <?php
+// API sometimes reports 403, so we need to prepare for that
+ error_reporting(E_ALL ^ E_WARNING);
  $url="http://172.0.200.108/aircon/get_sensor_info";
- $result = explode(",",file_get_contents($url));
+ $data = file_get_contents($url) or exit("API not responding\n");
 
+// exploding data into array
+ $result = explode(",",$data);
+
+// parsing array into separate variables
  foreach($result as $line)
  {
   $values = explode('=',$line);
   ${$values[0]} = trim($values[1]);
  }
+
+// sending data to syslog
  openlog("Daikin_0", LOG_PID | LOG_PERROR, LOG_LOCAL0);
- syslog(LOG_INFO, "daikin_inside_temp#".$htemp);
- syslog(LOG_INFO, "daikin_outside_temp#".$otemp);
- syslog(LOG_INFO, "daikin_power#".$mompow);
+ syslog(LOG_INFO, "daikin_inside_temp ".$htemp);
+ syslog(LOG_INFO, "daikin_outside_temp ".$otemp);
+ syslog(LOG_INFO, "daikin_power ".$mompow);
  closelog();
 ?>
